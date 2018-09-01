ff7ef01493ed12bac751435e0eab50e2e6877aa3
Evan Steiner <evanjs@rams.colostate.edu>
2018-09-01 15:32:31 -0600

    First Commit

---
+++ Transforming_Objects/Transforming_Objects/Transforming_Objects.cpp
@@ -0,0 +1,12 @@
+﻿// Transforming_Objects.cpp : Defines the entry point for the application.
+//
+
+#include "Transforming_Objects.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
