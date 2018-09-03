f58cc38856ee88545fd6cdccda9f7ce0d261dcb1
Evan Steiner <evanjs@rams.colostate.edu>
2018-09-03 11:42:40 -0600

    added testing files and file handler

---
+++ Transforming_Objects/Source/3DObjects.cpp
@@ -0,0 +1,11 @@
+class 3DObjects{
+	public:
+		3DObjects(int[] vertices, int[] faces) : vertices(vertices), faces(faces){
+
+		}
+
+	private:
+		int[] vertices;
+		int[] faces;
+
+}
---
+++ Transforming_Objects/Source/Transforming_Objects.cpp
@@ -0,0 +1,18 @@
+﻿// Transforming_Objects.cpp : Defines the entry point for the application.
+//
+
+#include "Transforming_Objects.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+	if (argc == 2) {
+		string filename = argv[1];
+		cout << "Filename: " << filename << endl;
+		return 0;
+	}
+	return 0;
+}
